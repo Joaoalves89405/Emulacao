6a7fc5f2e7b378382c7831d87317dfbe71eae360
Joao Alves <jmca-2000@hotmail.com>
2021-12-01 16:19:54 +0000

    Datagramas

--- codigo_teste/peer_node.py
+++ codigo_teste/peer_node.py
@@ -3,29 +3,45 @@ import time
 import db_access
 from threading import Thread
 
-
-serverIP = socket.gethostbyname(socket.gethostname())
 MCAST_IP = '224.0.0.1'
 MCAST_PORT = 5001
 MULTICAST_TTL = 2
 
-
-def broadcast_Message( message):
-	socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			#DATAGRAMA HELLO
+#	|	IP	|	RAZAO(0)	|	TIMESTAMP 
+def hello_packet (socket_UDP):
 	socket_UDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-
+	localIP=socket.gethostbyname(socket.gethostname())
+	razao = 0
 	timestamp = time.time()
-	udp_data = (message + str(timestamp)).encode()
+	hello = (localIP + str(razao)+ str(timestamp)).encode()
+	socket_UDP.sendto(hello,(MCAST_IP,MCAST_PORT))
 
-	socket_UDP.sendto(udp_data, (MCAST_IP,MCAST_PORT))
+			#DATAGRAMA GOODBYE
+#	|	IP	|	RAZAO(1)	|	TIMESTAMP 
+def goodbye_packet (socket_UDP):
+	socket_UDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+	localIP=socket.gethostbyname(socket.gethostname())
+	razao = 1
+	timestamp = time.time()
+	hello = (localIP + str(razao)+ str(timestamp)).encode()
+	socket_UDP.sendto(hello,(MCAST_IP,MCAST_PORT))	
 
+			#DATAGRAMA ERROR
+#	|	IP	|	RAZAO(2)	|	TIMESTAMP 
+def error_packet (socket_UDP):
+	socket_UDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+	localIP=socket.gethostbyname(socket.gethostname())
+	razao = 2
+	timestamp = time.time()
+	hello = (localIP + str(razao)+ str(timestamp)).encode()
+	socket_UDP.sendto(hello,(MCAST_IP,MCAST_PORT))		
 
 
 if __name__ == '__main__':
+	socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	hello_packet(socket_UDP)
+	goodbye_packet(socket_UDP)
 
-	for x in range(5):
-		val = input('Enter message to send:')
-		broadcast_Message(str(val))
-		pass
 
 	socket_UDP.close()
