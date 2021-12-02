d76ff8692137480c4acc8ac58b6c9ad1a4b0a025
jpmm98 <zejunior98@gmail.com>
2021-12-02 15:51:17 +0000

    Peer node update

--- codigo_teste/peer_node.py
+++ codigo_teste/peer_node.py
@@ -10,8 +10,7 @@ localIP=socket.gethostbyname(socket.gethostname())
 
 			#DATAGRAMA HELLO
 #	|	IP	|	RAZAO(0)	|	TIMESTAMP 
-def hello_packet (socket_UDP):
-	socket_UDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+def send_hello_packet (socket_UDP):
 	razao = 0
 	timestamp = time.time()
 	hello = (localIP + str(razao)+ str(timestamp)).encode()
@@ -36,14 +35,29 @@ def error_packet (socket_UDP):
 	hello = (localIP + str(razao)+ str(timestamp)).encode()
 	socket_UDP.sendto(hello,(MCAST_IP,MCAST_PORT))		
 
+def boot_up():
+	#> Entering Overlay network
+	#> send Hello packet
+	#> receive response treat each one
+	#> Case server > 
+		#> Create database 
+		#> Add peer0
+	#> Case peer >
+		#> Hold until DB is created
+		#> add to DB
+
+	socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	socket_UDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+	send_hello_packet(socket_UDP)
+
+	
 
 
+	pass	
 
 
 if __name__ == '__main__':
-	socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	hello_packet(socket_UDP)
-
+	
 	goodbye_packet(socket_UDP)
 
 
